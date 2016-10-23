1410c5ed4358e6461c9ba36f24fd21c6e34e2b16
Piyush Deshpande <d.piyush@rocketmail.com>
2016-10-23 23:24:18 +0530

    Separate implmentation of dom from main.rs

---
+++ src/dom.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+
+pub type AttrMap = HashMap<String, String>;
+
+#[derive(Debug)]
+pub struct Node {
+    // data for all nodes
+    pub children: Vec<Node>,
+
+    // type of the node
+    pub node_type: NodeType,
+}
+
+#[derive(Debug)]
+pub enum NodeType {
+    Text(String),
+    Element(ElementData),
+}
+
+#[derive(Debug)]
+pub struct ElementData {
+    pub tag_name: String,
+    pub attributes: AttrMap,
+}
+
+
+pub fn text(data: String) -> Node {
+    Node { children: Vec::new(), node_type: NodeType::Text(data) }
+}
+
+pub fn elem(children: Vec<Node>, tag: String, attr: AttrMap) -> Node {
+    Node {
+        children: children,
+        node_type: NodeType::Element(ElementData {
+            tag_name: tag,
+            attributes: attr
+        })
+    }
+}
+
+impl Node {
+    pub fn print(&self) {
+        self.node_type.print();
+        for i in &self.children {
+            i.print();
+        }
+    }
+}
+
+impl NodeType {
+    pub fn print(&self) {
+        match *self {
+            NodeType::Text(ref expr) => println!("{}", expr),
+            NodeType::Element(ref expr) => println!("<{}>", expr.tag_name),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,9 @@
-use std::collections::HashMap;
-
-type AttrMap = HashMap<String, String>;
-
-#[derive(Debug)]
-struct Node {
-    // data for all nodes
-    children: Vec<Node>,
-
-    // type of the node
-    node_type: NodeType,
-}
-
-#[derive(Debug)]
-enum NodeType {
-    Text(String),
-    Element(ElementData),
-}
-
-#[derive(Debug)]
-struct ElementData {
-    tag_name: String,
-    attributes: AttrMap,
-}
-
-
-fn text(data: String) -> Node {
-    Node { children: Vec::new(), node_type: NodeType::Text(data) }
-}
-
-fn elem(children: Vec<Node>, tag: String, attr: AttrMap) -> Node {
-    Node {
-        children: children,
-        node_type: NodeType::Element(ElementData {
-            tag_name: tag,
-            attributes: attr
-        })
-    }
-}
-
-impl Node {
-    fn print(&self) {
-        self.node_type.print();
-        for i in &self.children {
-            i.print();
-        }
-    }
-}
-
-impl NodeType {
-    fn print(&self) {
-        match *self {
-            NodeType::Text(ref expr) => println!("{}", expr),
-            NodeType::Element(ref expr) => println!("<{}>", expr.tag_name),
-        }
-    }
-}
+pub mod dom;
 
 fn main() {
-    let mut root = elem(Vec::new(), "html".to_string(), AttrMap::new());
-    let mut body = elem(Vec::new(), "body".to_string(), AttrMap::new());
-    body.children.push(text("Hello World".to_string()));
+    let mut root = dom::elem(Vec::new(), "html".to_string(), dom::AttrMap::new());
+    let mut body = dom::elem(Vec::new(), "body".to_string(), dom::AttrMap::new());
+    body.children.push(dom::text("Hello World".to_string()));
     root.children.push(body);
     root.print();
 }
