58aa80e6059f8b67d20b8c3d1d378a392ce6fc14
Piyush Deshpande <d.piyush@rocketmail.com>
2016-11-07 00:13:25 +0530

    Update code to add File read functionality

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 pub mod dom;
 pub mod parser;
 
-fn main() {
-    let html: String = "<html>
-                            <body>
-                                <h1>Title</h1>
-                                <div id='main' class='test'>
-                                    <p>Hello <em>world</em>!</p>
-                                </div>
-                            </body>
-                        </html>".to_string();
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
 
+fn main() {
+    let file_path = "example/sample.html".to_string();
+    let html = file_to_string(file_path);
     let root = parser::parse(html);
     root.print();
+}
+
+fn file_to_string(file_path :String) -> String {
+    let mut file = match File::open(file_path) {
+        Err(why) => panic!("couldn't open {}", why.description()),
+        Ok(file) => file,
+    };
+
+    let mut html_string = String::new();
+    match file.read_to_string(&mut html_string){
+        Err(why) => panic!("couldn't read {}", why.description()),
+        Ok(_) => println!("File read success!"),
+    };
+    html_string
 }
